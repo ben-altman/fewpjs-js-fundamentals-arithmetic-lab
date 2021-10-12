d913713c7753de736b5cc7316c46d8f2f86c702e
ben-altman <60247694+ben-altman@users.noreply.github.com>
2021-10-11 23:43:15 -0400

    commit tree 9e99afc884504ad87bdcc165b667f3144e977c4c

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = Math.ceil(Math.random() * 10)
+
+const num3 = 5
+const num4 = 24
+const mod = num4 % num3
+
+const max = Math.max(4, 20, 12, 9, 0)
